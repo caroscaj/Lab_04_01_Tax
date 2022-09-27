e835a2055a108692a86e61915edc7efe837afc6f
caroscaj <caroscaj@mail.uc.edu>
2022-09-27 14:56:52 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+
+        double pricePurchase = 5.75;
+        double salesTax = 0.05;
+        System.out.println("The price of the purchase is" + " " + pricePurchase);
+        System.out.println("The sales tax is " + salesTax);
+    }
+}
